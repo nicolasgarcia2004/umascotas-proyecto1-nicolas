1f4ec0a5d0186f1513c235c66f4fbe51b1901bc2
Nicolas Esteban Garcia Tamayo <132920613+nicolasgarcia2004@users.noreply.github.com>
2025-11-12 14:30:46 -0500

    Initial commit: Proyecto U-Mascota - Sistema de Adopción (Spring Boot + MySQL)

---
+++ src/main/java/com/example/umascota/Umascota2Application.java
@@ -0,0 +1,13 @@
+package com.example.umascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umascota2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umascota2Application.class, args);
+	}
+
+}
